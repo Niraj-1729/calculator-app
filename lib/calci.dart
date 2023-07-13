b52307176a18c917c2dea1356be8b103977ca119
Niraj Kumar <nirajs9kr@gmail.com>
2023-07-13 16:53:41 +0530

    Working

--- lib/calci.dart
+++ lib/calci.dart
@@ -1,3 +1,7 @@
+import 'dart:ui';
+
+import 'package:calculator/widget%5D/btn.dart';
+import 'package:calculator/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -12,9 +16,77 @@ class CalculatorApp extends StatefulWidget {
 class _CalculatorAppState extends State<CalculatorApp> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: Container(
-              
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Expanded(child: Container(color: Colors.black)),
+          Row(
+            children: [
+              button(text: "AC",butttonBgColor: operatorColor),
+              button(text: "<",butttonBgColor: operatorColor),
+              button(text: "",butttonBgColor: Colors.transparent),
+              button(text: "/"),
+            ],
+          ),
+          Row(
+            children: [
+              button(text: "7"),
+              button(text: "8"),
+              button(text: "9"),
+              button(text: "x"),
+            ],
+          ),
+          Row(
+            children: [
+              button(text: "4"),
+              button(text: "5"),
+              button(text: "6"),
+              button(text: "-"),
+            ],
+          ),
+          Row(
+            children: [
+              button(text: "1"),
+              button(text: "2"),
+              button(text: "3"),
+              button(text: "+"),
+            ],
+          ),
+          Row(
+            children: [
+              button(text: "%"),
+              button(text: "0"),
+              button(text: "."),
+              button(text: "="),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget button({text, tcolor = Colors.white, butttonBgColor = buttonColor}) {
+    return Expanded(
+      child: Container(
+        margin: const EdgeInsets.all(8),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+            backgroundColor: butttonBgColor,
+            padding: const EdgeInsets.all(22),
+          ),
+          onPressed: () {},
+          child: Text(
+            text,
+            style: TextStyle(
+              fontSize: 18,
+              color: tcolor,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
       ),
     );
   }
---
+++ lib/widget]/btn.dart
@@ -0,0 +1,38 @@
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/src/widgets/framework.dart';
+// import 'package:flutter/src/widgets/placeholder.dart';
+
+// import '../colors.dart';
+
+// class Button extends StatelessWidget {
+//   const Button({super.key});
+  
+
+//   @override
+//   Widget build(BuildContext context) {
+    
+//     return Expanded(
+//       child: Container(
+//         margin: const EdgeInsets.all(8),
+//         child: ElevatedButton(
+//           style: ElevatedButton.styleFrom(
+//             shape:
+//                 RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+//             backgroundColor: buttonColor,
+//             padding: const EdgeInsets.all(22),
+//           ),
+//           onPressed: () {},
+//           child: const Text(
+//             "1",
+//             style: TextStyle(
+//               fontSize: 18,
+//               color: Colors.white,
+//               fontWeight: FontWeight.bold,
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
